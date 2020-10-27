332ce6abb7487a6ee7f3802ba7e27a4871fadd99
Arjios <antonio.uab.ios.fisica@gmail.com>
2020-10-26 23:20:15 -0300

    Page Search

--- src/pages/Search/indexSearch.tsx
+++ src/pages/Search/indexSearch.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from '../../core/components/Button/indexButton';
 
 import './stylesSearch.css';
 
 const Search = () => {
     return (
-        <div>
-            <h1>Page SEARCH</h1>
+        <div className="search-form-card">
+            <div className="search-form-title">
+                Encontre um perfil Github
+            </div>
+            <div className="search-form-input">
+                <input type="text" className="search-form-input-text"/>
+            </div>
+            <div className="search-form-button">
+                <Link to="/Search" className="search-form-button-text">
+                    <Button indexButtonText="Encontrar" />
+                </Link>
+            </div>
         </div>
     )
 }
